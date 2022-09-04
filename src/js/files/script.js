381e24330fdf4c68a81a9dd649d8711cb7cad8c3
Sergey Sirotkin <sirotkin.san@gmail.com>
2022-09-04 12:11:09 +0300

    Header has been created

---
+++ src/js/files/script.js
@@ -0,0 +1,7 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile, _slideToggle } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+
+
