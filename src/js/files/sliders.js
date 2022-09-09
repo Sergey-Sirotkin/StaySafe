577e22b33ff65ef62fcf927c670aab628a318116
Sergey Sirotkin <sirotkin.san@gmail.com>
2022-09-09 14:02:17 +0300

    About us page has been created. Hero and statistics sections have been added.

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -176,6 +176,63 @@ function initSliders() {
 			}
 		});
 	}
+	if (document.querySelector('.about-us-slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.about-us-slider__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, Pagination],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 80,
+
+			//autoHeight: true,
+			speed: 800,
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Эффекты
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагинация
+			
+			pagination: {
+				el: '.about-us-slider__bullets',
+				clickable: true,
+			},
+			
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.swiper-button-prev',
+				nextEl: '.swiper-button-next',
+			},
+
+			// События
+			on: {
+
+			}
+		});
+	}
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
